e68858643121e2657be9d4891d54173d8f6a37be
div-ang <angapaydivin@gmail.com>
2021-01-05 20:28:34 +0000

    ajout des getters et setters dans todo-item

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,15 +1,50 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit,ElementRef} from '@angular/core';
+import { TodoService } from '../todo.service';
 
+import { TodoItemData } from '../dataTypes/TodoItemData';
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+  private theItem :TodoItemData;
+  private inputLabel: ElementRef;
+  get getLabel(): string {
+    return this.theItem.label;
+  }
 
-  constructor() { }
+  set setLabel(lab: string) {
+    this.todoService.setItemsLabel(lab, this.theItem);
+  }
+  private EnEdition = false;
+  constructor( private todoService: TodoService) { }
+  get getEdition(): boolean {
+    return this.EnEdition;
+  }
+
+  set setEdition(editionArg: boolean) {
+    this.EnEdition= editionArg;
+    requestAnimationFrame(() => this.inputLabel.nativeElement.focus());
+  }
 
   ngOnInit() {
   }
+  get label(): string {
+    return this.theItem.label;
+  }
+
+  set label(labelArg: string) {
+    this.todoService.setItemsLabel(labelArg, this.theItem);
+  }
+  setItemsDone(item: TodoItemData, done: boolean) {
+    this.todoService.setItemsDone(done, item);
+  }
+
+  removeItems() {
+    this.todoService.removeItems(this.theItem);
+  }
+  
+
 
 }
